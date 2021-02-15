8df9e65235e970282c05f0f4741bbd299ab7f291
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-02-15 20:09:39 +0900

    Create videoStream

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use('/peerjs', peerServer);
 app.get('/', (req, res) => {
   res.redirect(`/${uuidv4()}`);
 });
@@ -18,9 +23,9 @@ app.get('/:room', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('join-room', (roomId) => {
+  socket.on('join-room', (roomId, userId) => {
     socket.join(roomId);
-    socket.to(roomId).broadcast.emit('user-connected');
+    socket.to(roomId).broadcast.emit('user-connected', userId);
   });
 });
 

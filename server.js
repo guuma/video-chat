1b7dec67b91873bfe1e73cf393ca314ee02f3f1c
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-02-15 18:09:55 +0900

    Add logic for connet to new user

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
+const { Socket } = require('dgram');
+const { render } = require('ejs');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('room');
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected');
+  });
 });
 
 server.listen(3030);

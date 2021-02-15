656787bf2e8d8c9343d54948267234a4b125e8ce
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-02-15 18:07:06 +0900

    Add script.js and style.css

---
+++ public/script.js
@@ -0,0 +1,36 @@
+// const { Socket } = require('socket.io');
+const socket = io('/');
+
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+let myVideoStream;
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+  });
+
+socket.emit('join-room', ROOM_ID);
+
+socket.on('user-connected', () => {
+  connecToNewUser();
+});
+
+const connecToNewUser = () => {
+  console.log('new user');
+}
+
+const addVideoStream = (video, stream) => {
+  console.log(stream);
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
